e17e0111f93688832f8c4e218ada6295583aa2e8
Mike Fotinakis <mike@fotinakis.com>
2016-11-30 09:48:10 -0800

    Fix hostname bug when no port exists.

--- helpers.go
+++ helpers.go
@@ -18,7 +18,7 @@ const animationStopperHtml = `<style type="text/css">
 
 // Transforms localhost requests into requests that will be reverse proxied based on the proxymap.
 func proxifyIfLocalRequest(req *http.Request) {
-	hostname, _, _ := net.SplitHostPort(req.URL.Host)
+	hostname := justHostname(req.URL.Host)
 	if hostname == "localhost" || hostname == "127.0.0.1" {
 		req.Host = *proxymeHostnameFlag
 		req.URL.Host = *proxymeHostnameFlag
