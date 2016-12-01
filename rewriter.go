937e8c31af0beb4db4c4ac86d5c3f5f6d3650acb
Mike Fotinakis <mike@fotinakis.com>
2016-12-01 10:41:39 -0800

    Set X-Forwarded-For.

--- rewriter.go
+++ rewriter.go
@@ -4,6 +4,7 @@ import (
 	"github.com/vulcand/oxy/forward"
 	"github.com/vulcand/oxy/utils"
 	"net/http"
+	"net"
 )
 
 type customRewriter struct{}
@@ -12,6 +13,10 @@ func (rw *customRewriter) Rewrite(req *http.Request) {
 	// TODO: this is a hack for making oxy copy the request correctly when the path changes.
 	req.URL.Opaque = ""
 
+	// Set X-Forwarded-For header.
+	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+		req.Header.Set("X-Forwarded-For", clientIP)
+	}
 	// Remove hop-by-hop headers to the backend.  Especially important is "Connection" because we
 	// want a persistent connection, regardless of what the client sent to us.
 	utils.RemoveHeaders(req.Header, forward.HopHeaders...)
